572204e886dc122f3e7e34c77159b878efbc89d6
Herman <herman.papko@gmail.com>
2026-02-17 10:50:01 +0100

    task 150 resolved

---
+++ EvaluateReversePolishNotation.php
@@ -0,0 +1,28 @@
+<?php
+
+class EvaluateReversePolishNotation
+{
+    function evalRPN($tokens) {
+        $operators = ['+', '-', '*', '/'];
+        $stack = [];
+        foreach ($tokens as $token) {
+            if (in_array($token, $operators)) {
+                $b = array_pop($stack);
+                $a = array_pop($stack);
+
+                $result = match ($token) {
+                    '+' => $a + $b,
+                    '-' => $a - $b,
+                    '*' => $a * $b,
+                    '/' => intval($a / $b),
+                };
+
+                array_push($stack, $result);
+            } else {
+                array_push($stack, (int)$token);
+            }
+        }
+        return array_pop($stack);
+
+    }
+}
