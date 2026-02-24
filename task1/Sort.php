8b76444824f4287e6623d815267f16ade50a571d
Herman <herman.papko@gmail.com>
2026-02-24 14:03:47 +0100

    $

--- task1/Sort.php
+++ task1/Sort.php
@@ -10,7 +10,7 @@ class Sort
             for ($j = 0; $j < $len - $i - 1; $j++) {
                 if ($arr[$j] > $arr[$j + 1]) {
                     $temp = $arr[$j];
-                    $arr[$j] = $arr[j + 1];
+                    $arr[$j] = $arr[$j + 1];
                     $arr[$j + 1] = $temp;
                 }
             }
